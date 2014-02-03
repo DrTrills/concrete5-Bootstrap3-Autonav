34194828495ce5faa97bbfe8cd5f16f04e61dbeb
pj <patrickjamal@gmail.com>
2014-02-03 01:59:34 -0500

    copy to blocks folder.

---
+++ blocks/autonav/templates/header_menu.php
@@ -0,0 +1,27 @@
+<?php  defined('C5_EXECUTE') or die(_("Access Denied."));
+$navItems = $controller->getNavItems();
+?>
+<!-- CUSTOM NAV -->
+
+<?php  foreach ($navItems as $ni) {
+	
+	$classes = array();
+	if ($ni->isCurrent) {
+		$classes[] = 'nav-selected';
+	}
+	if ($ni->inPath) {
+		$classes[] = 'nav-path-selected';
+	}
+	if ($ni->isFirst) {
+		$classes[] = 'first';
+	}
+	$classes = implode(" ", $classes);
+	?>
+	
+	<li class="<?php echo $classes?>">
+		<a class="<?php echo $classes?>" href="<?php echo $ni->url?>" target="<?php echo $ni->target?>"><?php echo $ni->name?></a>
+	</li>
+<?php  } ?>
+
+
+<div class="ccm-spacer">&nbsp;</div>
